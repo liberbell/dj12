d5abe1e1939b42fcbafb6355c7a5117f702932ee
eliberbell <liberbell@gmail.com>
2026-07-16 13:21:21 +0900

    views2

--- rearrysite/mysite/views.py
+++ rearrysite/mysite/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello django world")
+    return HttpResponse("<h1>hello django world</h1>")
