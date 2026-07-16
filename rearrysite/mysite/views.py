a55562b7496aca55522c75d59a26f9eb79ff0351
eliberbell <liberbell@gmail.com>
2026-07-17 08:25:18 +0900

    views3

--- rearrysite/mysite/views.py
+++ rearrysite/mysite/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    context = {
+        "title": "Really origin site"
+    }
     return render(request, "mysite/index.html", {})
