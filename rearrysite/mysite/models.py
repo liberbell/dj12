65c5e1fd7b890696645dc8e8c12ed070bba76711
eliberbell <liberbell@gmail.com>
2026-07-22 08:34:33 +0900

    models1

--- rearrysite/mysite/models.py
+++ rearrysite/mysite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError()
+
+
+class create_user(self, email, password=None):
+    pass
