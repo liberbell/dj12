eaeba141c5e8c374502834e77080555cdce811a8
eliberbell <liberbell@gmail.com>
2026-07-22 08:40:26 +0900

    models3

--- rearrysite/mysite/models.py
+++ rearrysite/mysite/models.py
@@ -10,4 +10,14 @@ class UserManager(BaseUserManager):
             email = self.normalize_email(email)
         )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
