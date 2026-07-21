b84c0bb37ce2bdd4584c1a39f947f1134584f4b1
eliberbell <liberbell@gmail.com>
2026-07-21 12:05:33 +0900

    models2

--- rearrysite/blog/models.py
+++ rearrysite/blog/models.py
@@ -5,3 +5,5 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(max_length=1024)
     author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
