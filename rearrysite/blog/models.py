81ef6a064c0316005f6740f69788aca6834cadb1
eliberbell <liberbell@gmail.com>
2026-07-22 08:28:10 +0900

    models1

--- rearrysite/blog/models.py
+++ rearrysite/blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField(max_length=1024)
-    author = models.CharField(max_length=255)
+    title = models.CharField(default="", max_length=255)
+    text = models.TextField(default="", max_length=1024)
+    author = models.CharField(default="", max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
