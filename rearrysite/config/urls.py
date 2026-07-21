5b22595028c9eb496d55618d24e45e71c4d182bb
eliberbell <liberbell@gmail.com>
2026-07-21 11:54:01 +0900

    urls2

--- rearrysite/config/urls.py
+++ rearrysite/config/urls.py
@@ -21,5 +21,5 @@ from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
