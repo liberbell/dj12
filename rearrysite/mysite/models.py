6764634357049b8ce584395fc4f7f0f200923610
eliberbell <liberbell@gmail.com>
2026-07-22 08:48:38 +0900

    models6

--- rearrysite/mysite/models.py
+++ rearrysite/mysite/models.py
@@ -29,3 +29,7 @@ class User(AbstractBaseUser):
     )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
