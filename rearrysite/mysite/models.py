4ecc9d8dfc7fdb33f1a3b1c4e965229ecc186260
eliberbell <liberbell@gmail.com>
2026-07-22 12:55:21 +0900

    models9

--- rearrysite/mysite/models.py
+++ rearrysite/mysite/models.py
@@ -38,6 +38,10 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
     
-    def has_parm(self):
+    def has_parms(self):
         "Does the user have a specific permission?"
+        return True
+    
+    def has_module_parms(self):
+        "Does the user have permissions to view the app `app_label?`"
         return True
