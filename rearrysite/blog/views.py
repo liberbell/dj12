dacdbcbb9400643e9feb2d1c6b15a22c14866e24
eliberbell <liberbell@gmail.com>
2026-07-25 11:10:00 +0900

    views3

--- rearrysite/blog/views.py
+++ rearrysite/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 
 # Create your views here.
 def article(request, pk):
+    obj = Article.objects.get(pk=pk)
     context = {
-        "pk": pk,
+        "article": obj,
     }
     
     return render(request, "blog/article.html", context=context)
