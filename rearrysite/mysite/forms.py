d8b0f2ad271c3f07d1b6473701e441251d862c29
eliberbell <liberbell@gmail.com>
2026-08-02 23:14:48 +0900

    forms2

--- rearrysite/mysite/forms.py
+++ rearrysite/mysite/forms.py
@@ -12,3 +12,9 @@ class UserCreationForm(forms.ModelForm):
         password = self.cleaned_data.get["password"]
         return password
     
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
