6e1a539d285574647829d884d2078327270853b6
eliberbell <liberbell@gmail.com>
2026-08-02 23:10:29 +0900

    forms1

--- rearrysite/mysite/forms.py
+++ rearrysite/mysite/forms.py
@@ -3,3 +3,12 @@ from django.contrib.auth import get_user_model
 
 class UserCreationForm(forms.ModelForm):
     password = forms.CharField()
+    
+    class Meta:
+        model = get_user_model()
+        fields = "email"
+        
+    def clean_password(self):
+        password = self.cleaned_data.get["password"]
+        return password
+    
