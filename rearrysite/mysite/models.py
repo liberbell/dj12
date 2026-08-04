140b9765a553298acba4965b7dbb0bd01d091830
eliberbell <liberbell@gmail.com>
2026-08-04 21:53:20 +0900

    models1

--- rearrysite/mysite/models.py
+++ rearrysite/mysite/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
-            raise ValueError("Useer must have an email address")
+            raise ValueError("User must have an email address")
         user = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
         )
         user.set_password(password)
         user.save(using=self._db)
